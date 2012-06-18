32afffc051d09564f3040852b8bff0c9b10ddbab
NTTdocomo <igarashi@igarashis-MacBook-Pro.local>
2012-06-18 20:42:47 +0900

    revert host_manager

--- nova/nova/scheduler/host_manager.py
+++ nova/nova/scheduler/host_manager.py
@@ -1,4 +1,3 @@
-# Copyright (c) 2012 NTT DOCOMO, INC. 
 # Copyright (c) 2011 OpenStack, LLC.
 # All Rights Reserved.
 #
@@ -29,13 +28,6 @@ from nova.openstack.common import cfg
 from nova.scheduler import filters
 from nova import utils
 
-""" start add by NTT DOCOMO """
-import pprint
-LOG = logging.getLogger(__name__)
-PP=pprint.PrettyPrinter(indent=4)
-from nova import context as ctx
-import operator
-""" end add by NTT DOCOMO """
 
 host_manager_opts = [
     cfg.IntOpt('reserved_host_disk_mb',
@@ -107,26 +99,15 @@ class HostState(object):
     def __init__(self, host, topic, capabilities=None, service=None):
         self.host = host
         self.topic = topic
-                
+
         # Read-only capability dicts
 
         if capabilities is None:
             capabilities = {}
         self.capabilities = ReadOnlyDict(capabilities.get(topic, None))
-        
-        """ start add by NTT DOCOMO """
-        self.available_hosts = []
-        self.physical_compute = False
-        
-        extra_type = self.capabilities.get('type', None)
-        if extra_type == "physical":
-            self.physical_compute = True
-        """ end add by NTT DOCOMO """
-        
         if service is None:
             service = {}
         self.service = ReadOnlyDict(service)
-        
         # Mutable available resources.
         # These will change as resources are virtually "consumed".
         self.free_ram_mb = 0
@@ -134,97 +115,27 @@ class HostState(object):
         self.vcpus_total = 0
         self.vcpus_used = 0
 
-    """ start add by NTT DOCOMO """
-    def update_from_compute_node(self, compute, context=None):
+    def update_from_compute_node(self, compute):
         """Update information about a host from its compute_node info."""
-        
-        if self.physical_compute:
-            phy_hosts = db.phy_host_get_all_by_service_id(context, compute['service_id'])
-            LOG.debug("point0.1: self=%s, phy_hosts=%s", str(self), PP.pformat(phy_hosts))
-            
-            for host in phy_hosts:
-                if not host.instance_id:
-                    self.available_hosts.append(host)
-                    
-            """those sorting should be decided by weight in a scheduler """
-            self.available_hosts = sorted(self.available_hosts, key=operator.attrgetter('memory_mb'), reverse=True)
-            self.available_hosts = sorted(self.available_hosts, key=operator.attrgetter('cpus'), reverse=True)
-            
-            LOG.debug("point0.2: self=%s available_hosts=%s", str(self), PP.pformat(self.available_hosts))
-              
-            if len(self.available_hosts):
-                phy_host = self.available_hosts[0]
-                LOG.debug("point0.2.1: phy_hosts=%s", PP.pformat(phy_host))
-            else:
-                phy_host = {}
-                phy_host['local_gb'] = 0
-                phy_host['memory_mb'] = 0
-                phy_host['cpus'] = 0
-                
-            all_disk_mb = phy_host['local_gb'] * 1024
-            all_ram_mb = phy_host['memory_mb']
-            vcpus_total = phy_host['cpus']
-        
-        else: 
-            all_disk_mb = compute['local_gb'] * 1024
-            all_ram_mb = compute['memory_mb']
-            vcpus_total = compute['vcpus']
-            if FLAGS.reserved_host_disk_mb > 0:
-                all_disk_mb -= FLAGS.reserved_host_disk_mb
-            if FLAGS.reserved_host_memory_mb > 0:
-                all_ram_mb -= FLAGS.reserved_host_memory_mb
-            
+        all_disk_mb = compute['local_gb'] * 1024
+        all_ram_mb = compute['memory_mb']
+        vcpus_total = compute['vcpus']
+        if FLAGS.reserved_host_disk_mb > 0:
+            all_disk_mb -= FLAGS.reserved_host_disk_mb
+        if FLAGS.reserved_host_memory_mb > 0:
+            all_ram_mb -= FLAGS.reserved_host_memory_mb
         self.free_ram_mb = all_ram_mb
         self.free_disk_mb = all_disk_mb
         self.vcpus_total = vcpus_total
-        
-        LOG.debug("point0.3: self=%s free_ram_mb=%f free_disk_mb=%f vcpus_total=%f", str(self), self.free_ram_mb, self.free_disk_mb, self.vcpus_total)
-        
-        """ end add by NTT DOCOMO """
-        
+
     def consume_from_instance(self, instance):
         """Update information about a host from instance info."""
-        
-        """ start add by NTT DOCOMO """
-        if self.physical_compute:
-            LOG.debug("instance=%s", str(instance))
-            context = ctx.get_admin_context()
-            instance_id = instance.get('id', None)
-            if instance_id:
-                phy_host = db.phy_host_get_by_instance_id(context, instance['id'])
-            else:
-                phy_host = None    
-                
-            if phy_host:
-                return
-            elif len(self.available_hosts):
-                consumed_host = self.available_hosts.pop()
-                LOG.debug("point1.0.1: self=%s consumed_host=%s", str(self), PP.pformat(consumed_host.__dict__))
-            if len(self.available_hosts):
-                phy_host = self.available_hosts[0]
-                LOG.debug("point1.0.2: self=%s phy_hosts=%s", str(self), PP.pformat(phy_host.__dict__))
- 
-            else:
-                phy_host = {}
-                phy_host['local_gb'] = 0
-                phy_host['memory_mb'] = 0
-                phy_host['cpus'] = 0
-            
-            self.free_disk_mb = phy_host['local_gb'] * 1024
-            self.free_ram_mb = phy_host['memory_mb']
-            self.vcpus_used = 0
-            self.vcpus_total = phy_host['cpus']
-        else:
-            disk_mb = (instance['root_gb'] + instance['ephemeral_gb']) * 1024
-            ram_mb = instance['memory_mb']
-            vcpus = instance['vcpus']
-            self.free_ram_mb -= ram_mb
-            self.free_disk_mb -= disk_mb
-            self.vcpus_used += vcpus
-            
-        LOG.debug("point1.1: self=%s free_ram_mb=%f free_disk_mb=%f vcpus_total=%f", str(self), self.free_ram_mb, self.free_disk_mb, self.vcpus_total)
-        """ end add by NTT DOCOMO """
-        
+        disk_mb = (instance['root_gb'] + instance['ephemeral_gb']) * 1024
+        ram_mb = instance['memory_mb']
+        vcpus = instance['vcpus']
+        self.free_ram_mb -= ram_mb
+        self.free_disk_mb -= disk_mb
+        self.vcpus_used += vcpus
 
     def passes_filters(self, filter_fns, filter_properties):
         """Return whether or not this host passes filters."""
@@ -263,14 +174,6 @@ class HostManager(object):
     # Can be overriden in a subclass
     host_state_cls = HostState
 
-    """ start add by NTT DOCOMO """
-    """ Make HostManager to a singleton object """
-    def __new__(self):
-        if not hasattr(self, "__instance__"):
-            self.__instance__ = super(HostManager, self).__new__(self)
-        return self.__instance__
-    """ end add by NTT DOCOMO """
-    
     def __init__(self):
         self.service_states = {}  # { <host> : { <service> : { cap k : v }}}
         self.filter_classes = filters.get_filter_classes(
@@ -424,10 +327,7 @@ class HostManager(object):
             host_state = self.host_state_cls(host, topic,
                     capabilities=capabilities,
                     service=dict(service.iteritems()))
-            """ start add by DOCOMO """
-            # pass context to access DB
-            host_state.update_from_compute_node(compute, context=context)
-            """ end add by DOCOMO """
+            host_state.update_from_compute_node(compute)
             host_state_map[host] = host_state
 
         # "Consume" resources from the host the instance resides on.
